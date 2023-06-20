4156ddc2209851da225d981a0a036fb5cdc8ba41
= <inko51366@naver.com>
2023-06-20 05:30:32 +0000

    feat: 로그인 api 구현 (db연동)

--- server.js
+++ server.js
@@ -1,55 +1,44 @@
 const express = require("express");
 const app = express();
 
+// db connect
+const db = require("./database/connect/mariadb");
+db.connect();
+
 app.use(express.json());
 
-let userDB = [];
-
 // 로그인 api
+// GET
 app.get("/login", (req, res) => {
-  const { id, pw } = req.body;
+  const { id, pw } = req.query;
 
   const result = {
     "success": false,
     "message": ""
   }
 
-  if (id === "ehdtjs0612" && pw === "1234") {
-    result.success = true;
-    result.message = "로그인 성공";
+  db.query(
+    `SELECT id FROM user_TB WHERE login_id='${id}' AND password='${pw}'`,
+    (error, results, fields) => {
+      if (error) {
+        result.message = "데이터베이스 오류.";
+        res.send(result);
+        return;
+      }
 
-  } else {
-    result.message = "아이디 혹은 비밀번호가 올바르지 않습니다";
-  }
+      if (results.length === 1) {
+        result.success = true;
+        result.message = "로그인 성공";
+        
+      } else {
+        result.message = "아이디 혹은 비밀번호가 올바르지 않습니다.";
+      }
 
-  res.send(result);
+      res.send(result);
+    }
+  );
 });
 
-// 회원가입api
-//(아이디, 비밀번호, 이메일, 전화번호) body로 넘겨줌
-app.post("/signup", (req, res) => {
-  const { id, pw, email, phoneNumber } = req.body;
-
-  const result = {
-    "success": false,
-  }
-
-  // db연결 일단은 배열로 임시 db 만들자
-  const user = {
-    "id": id,
-    "pw": pw,
-    "email": email,
-    "phoneNumber": phoneNumber
-  };
-
-  userDB.push(user);
-  result.success = true;
-
-  res.send(userDB);
-})
-
-
-
 app.listen(8000, () => {
-  console.log("8000포트에서 실행");
+  console.log("8000번 포트에서 기다리는중");
 })
