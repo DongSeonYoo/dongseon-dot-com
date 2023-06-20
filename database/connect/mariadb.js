16fbd31d459f0f32ed37c8915466fe19caa17435
= <inko51366@naver.com>
2023-06-20 05:20:38 +0000

    remove: remove node_modules

---
+++ database/connect/mariadb.js
@@ -0,0 +1,11 @@
+const maria = require("mysql");
+
+const connect = maria.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'ehdtjs',
+  password: '1234',
+  database: 'yoo_DB'
+});
+
+module.exports = connect;
