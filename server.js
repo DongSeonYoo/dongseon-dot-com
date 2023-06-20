20a8af3a3afef0272d61a0ac971f969d507b4bf0
ehdtjs0612 <inko51366@naver.com>
2023-06-20 11:09:50 +0000

    feat: 회원가입 API 구현

--- server.js
+++ server.js
@@ -39,6 +39,45 @@ app.post("/login", (req, res) => {
   );
 });
 
+// 회원가입 api
+// id, pw, name, phone_number, email
+// POST
+app.post("/signup", (req, res) => {
+  const { id, pw, name, phoneNumber, email } = req.body;
+
+  const result = {
+    success: false,
+    message: "",
+  };
+
+  db.query(
+    `INSERT INTO user_TB (login_id, password, name, phone_number, email, created_date, updated_date) VALUES ('${id}', '${pw}', '${name}', '${phoneNumber}', '${email}', now(), now())`,
+    (error, results, fields) => {
+      if (error) {
+        const isDuplicateId = String(error).includes("login_id");
+        const isDuplicatePhoneNumber = String(error).includes("phone_number");
+        const isDuplicatedEmail = String(error).includes("email");
+
+        if (isDuplicateId) {
+          result.message = "중복된 아이디가 존재합니다";
+          
+        } else if (isDuplicatePhoneNumber) {
+          result.message = "중복된 전화번호가 존재합니다";
+          
+        } else if (isDuplicatedEmail) {
+          result.message = "중복된 이메일이 존재합니다";
+        }
+        
+      } else {
+        result.success = true;
+        result.message = "회원가입 성공";
+      }
+
+      res.send(result);
+    }
+  );
+});
+
 app.listen(8000, () => {
   console.log("8000번 포트에서 기다리는중");
 })
