d8087df4b733b3ca20290263d28169f36258619c
ehdtjs0612 <inko51366@naver.com>
2023-06-23 06:43:27 +0000

    feat: 게시글 수정 api

--- server.js
+++ server.js
@@ -408,7 +408,30 @@ app.patch("/post/:postId/content", (req, res) => {
 // userId, postId, title, content
 // PUT
 app.put("/post/:postId", (req, res) => {
+  const { postId } = req.params;
+  const { userId, title, content } = req.body;
+  const { query, params } = makeQuery("UPDATE post_TB SET title = ?, content = ? WHERE user_id = ? AND id = ?", [title, content, userId, postId]);
 
+  const result = makeResult();
+
+  db.query(query, params, (error, results, fields) => {
+    if (error) {
+      result.message = error.sqlMessage;
+      res.send(result);
+      return;
+    }
+
+    const data = results.affectedRows === 1;
+    if (data) {
+      result.success = true;
+      result.message = "수정 성공";
+
+    } else {
+      result.message = "수정 실패, 본인만 수정 가능";
+    }
+
+    res.send(result);
+  });
 });
 
 // 게시글 삭제
