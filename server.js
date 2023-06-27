ce45722d1b46e1970d8ec820602db6ca95fd1f8d
ehdtjs0612 <inko51366@naver.com>
2023-06-27 08:13:18 +0000

    refactor: api 라우팅 분리

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const app = express();
 
-// db connect
 const db = require("./database/connect/mariadb");
-db.connect();
-
-app.use(express.json());
 
 const accountApi = require("./router/account");
-app.use("/account", accountApi);
-
 const postApi = require("./router/post");
-app.use("/post", postApi);
-
 const commentApi = require("./router/comment");
+
+app.use("/account", accountApi);
+app.use("/post", postApi);
 app.use("/comment", commentApi);
 
-app.listen(8000, () => {
-  console.log("8000번 포트에서 기다리는중");
-});
+app.use(express.json());
+db.connect();
+
+module.exports = app;
