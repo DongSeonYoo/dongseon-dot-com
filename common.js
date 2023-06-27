2233b68cd69eb540ddb853e1c861afbe0cc325bf
ehdtjs0612 <inko51366@naver.com>
2023-06-27 01:34:03 +0000

    file: 전역 함수 분리

---
+++ common.js
@@ -0,0 +1,17 @@
+function printError(error, result, res) {
+  console.error(error.message);
+  result.message = error.sqlMessage;
+  res.send(result);
+};
+
+function makeResult() {
+  return {
+    success: false,
+    message: "",
+  };
+};
+
+module.exports = {
+  printError,
+  makeResult
+}
