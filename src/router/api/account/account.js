2ab015a5630311d639d5fe60951fad53816e5752
ehdtjs0612 <inko51366@naver.com>
2023-06-28 02:51:38 +0000

    refactor: api 컨트롤러 경로 연결 변수 수정account -> accountCtrl

--- src/router/api/account/account.js
+++ src/router/api/account/account.js
@@ -1,46 +1,46 @@
 const express = require("express");
 const router = express.Router();
 
-const account = require("../account/account.controller");
+const accountCtrl = require("../account/account.controller");
 
 // 로그인 api
 // loginId, pw
 // POST
-router.post("/login", account.login);
+router.post("/login", accountCtrl.login);
 
 // 회원가입 api
 // loginId, pw, name, phoneNumber, email
 // POST
-router.post("/signup",account.signup);
+router.post("/signup",accountCtrl.signup);
 
 // 아이디 찾기 api
 // name, phoneNumber, email
 // GET
-router.get("/loginId", account.findId);
+router.get("/loginId", accountCtrl.findId);
 
 // 비밀번호 찾기 api
 // 1.(사용자 인증 단계)
 // loginId, name, phoneNumber, email
-router.get("/pw", account.validateUser);
+router.get("/pw", accountCtrl.validateUser);
 
 // 비밀번호 찾기 api
 // 2.(비밀번호 재설정 단계)
 // userId, newPw
-router.post("/pw", account.resetPw);
+router.post("/pw", accountCtrl.resetPw);
 
 // 프로필 보기 api
 // userId
 // GET
-router.get("/:userId", account.viewProfile);
+router.get("/:userId", accountCtrl.viewProfile);
 
 // 회원 정보 수정 api
 // userId, name, phoneNumber, email
 // PUT
-router.put("/", account.modifyProfile);
+router.put("/", accountCtrl.modifyProfile);
 
 // 회원 탈퇴 api
 // userId
 // DELETE
-router.delete("/", account.deleteUser);
+router.delete("/", accountCtrl.deleteUser);
 
 module.exports = router;
