f6cdf1792811c3579b09862243568590917bdb44
ehdtjs0612 <inko51366@naver.com>
2023-07-01 08:15:02 +0000

    feat: 회원가입 페이지 유효성 검사

--- client/scripts/signup.js
+++ client/scripts/signup.js
@@ -1,86 +1,92 @@
+const idField = document.getElementById('id-text-field');
+const pwField = document.getElementById('pw-text-field');
+const pwCheckField = document.getElementById('pw-check-text-field');
+const nicknameField = document.getElementById('nickname-text-field');
+const emailField = document.getElementById('email-text-field');
+const phoneNumberField = document.getElementById('phonenumber-text-field');
+
 const loginIdRegex = /^[A-Za-z0-9]{8,15}$/;
 const pwRegex = /^.{10,17}$/;
 const nameRegex = /^[가-힣a-zA-Z]{2,8}$/;
 const phoneNumberRegex = /^0\d{10}$/;
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-function validate() {
-  const idField = document.getElementById('id-text-field');
-  const idRegex = /^[a-zA-Z0-9]{5,30}$/; // 아이디 정규식
+const validate = () => {
   if (idField.value === "") {
     alert("아이디를 입력해주세요");
     idField.focus();
-
     return false;
   }
 
-  if (!idRegex.test(idField.value)) {
-    alert("아이디는 영문자, 숫자 조합의 30자 이하의 문자열이어야 합니다.");
+  if (!loginIdRegex.test(idField.value)) {
+    alert("아이디는 영문자, 숫자 조합의 5~30자 이내 문자열이어야 합니다.");
     idField.focus();
-
     return false;
   }
-
-  const pwField = document.getElementById('pw-text-field');
-  const pwRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,25}$/; //비밀번호 정규식
 
   if (pwField.value === "") {
     alert("비밀번호를 입력해주세요");
     pwField.focus();
-
     return false;
   }
 
-  if (pwRegex.test(pwField.value)) {
-    alert('비밀번호는 영문자와 숫자 조합의 8 ~ 25자리 문자열이어야 합니다.');
+  if (!pwRegex.test(pwField.value)) {
+    alert("비밀번호는 영문자, 숫자, 특수문자 조합의 8~25자리 문자열이어야 합니다.");
     pwField.focus();
-
     return false;
   }
 
-  const pwCheckField = document.getElementById('pw-check-text-field');
   if (pwField.value !== pwCheckField.value) {
-    alert('비밀번호와 비밀번호 확인이 일치하지 않습니다.');
+    alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
     pwCheckField.focus();
-
     return false;
   }
 
-  const nicknameField = document.getElementById('nickname-text-field');
-  const nicknameRegex = /^[a-zA-Z가-힣0-9]{2,20}$/; // 닉네임 정규식
   if (nicknameField.value === "") {
     alert("닉네임을 입력해주세요");
     nicknameField.focus();
-
     return false;
   }
 
-  if (!nicknameRegex.test(nicknameField.value)) {
-    alert('닉네임은 영문자, 한글, 숫자 조합의 20자 이하의 문자열이어야 합니다. (최소 2글자)');
+  if (!nameRegex.test(nicknameField.value)) {
+    alert("닉네임은 영문자, 한글, 숫자 조합의 2~20자 이내 문자열이어야 합니다.");
     nicknameField.focus();
-
     return false;
   }
 
-  const emailField = document.getElementById('email-text-field');
-  const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/; // 이메일 정규식
+  if (phoneNumberField.value === "") {
+    alert("전화번호를 입력해주세요");
+    phoneNumberField.focus();
+    return false;
+  }
+
+  if (!phoneNumberRegex.test(phoneNumberField.value)) {
+    alert("전화번호는 010으로 시작하는 11자리 숫자 문자열이어야 합니다.");
+    phoneNumberField.focus();
+    return false;
+  }
+
   if (emailField.value === "") {
     alert("이메일을 입력해주세요");
     emailField.focus();
-
     return false;
   }
 
   if (!emailRegex.test(emailField.value)) {
-    alert('이메일 형식이 올바르지 않습니다.');
+    alert("유효한 이메일 주소를 입력해주세요");
     emailField.focus();
-
     return false;
   }
 
   return true;
+};
+
+const clickSignup = () => {
+  if (validate()) {
+    fetchData();
+  }
 }
 
-document.querySelector("#home-button").addEventListener('click', () => {
+document.querySelector("#home-button").addEventListener("click", () => {
   location.href = "/";
-})
+});
