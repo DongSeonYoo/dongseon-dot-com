e653bb42b4fefb5708ab2614ee09597d9dc54a91
ehdtjs0612 <inko51366@naver.com>
2023-07-03 08:40:59 +0000

    feat: 특정 게시글의 정보를 가져오는 api 호출하여 화면에 뿌려줌

--- client/scripts/post.js
+++ client/scripts/post.js
@@ -1,13 +1,43 @@
 const url = window.location.href.split("/");
 const pathVariable = url[url.length - 1];
 
-console.log(pathVariable);
+async function displayPost() {
+  const pathVariable = parseUrl();
+  const json = await specificPostFetch(pathVariable);
 
-specificPostFetch();
+  if (json.success) {
+    const post = json.message;
+    const postTitle = document.getElementById("post-title");
+    const postAuthor = document.getElementById("post-author");
+    const postContent = document.getElementById("post-content");
+    const postCreateDate = document.getElementById("post-create-date");
+    const postUpdateDate = document.getElementById("post-update-date");
+    
+    const parsingUpdatedDate = new Date(post.updated_date).toLocaleString();
+    const parsingCreateDate = new Date(post.created_date).toDateString();
 
-async function specificPostFetch() {
-  const res = await fetch("/api/post/" + pathVariable);
-  const json = await res.json();
+    postTitle.innerHTML = post.title;
+    postAuthor.innerHTML = "작성자: " + post.author_name;
+    postContent.innerHTML = post.content;
+    postCreateDate.innerHTML = "작성일: " + parsingUpdatedDate;
+    postUpdateDate.innerHTML = "최근 수정일: " + parsingCreateDate;
+  }
+}
 
-  console.log(json.message);
+function parseUrl() {
+  const url = window.location.href.split("/");
+  const pathVariable = url[url.length - 1];
+
+  return pathVariable;
+}
+
+async function specificPostFetch(pathVariable) {
+  try {
+    const result = await fetch("/api/post/" + pathVariable);
+
+    return await result.json();
+  } catch (error) {
+    alert("데이터베이스 오류: " + error);
+    console.error(error);
+  }
 }
