c64f5f7f037a2c5e78204fa2b2219eec1e7c8791
ehdtjs0612 <inko51366@naver.com>
2023-07-07 10:11:28 +0000

    commit: refactor: migration db comment api(createPost, readAllPost) (mariadb -> postgresql)

--- src/router/controller/comment.controller.js
+++ src/router/controller/comment.controller.js
@@ -1,10 +1,9 @@
-const db = require("../../database/connect/mariadb");
+const createClient = require("../../database/connect/postgresql");
 const { makeResult, printError } = require("../controller/util/func");
-
 const commentValidate = require("../controller/util/validate/commentValidate");
 const validateMessage = "데이터 형식이 유효하지 않습니다";
 
-const createComment = (req, res) => {
+const createComment = async (req, res) => {
   const { postId, userId, content } = req.body;
   const result = makeResult();
 
@@ -15,26 +14,30 @@ const createComment = (req, res) => {
     return;
   }
 
-  const sql = "INSERT INTO comment_TB (post_id, user_id, content) VALUES (?, ?, ?)";
-  const param = [postId, userId, content];
+  const client = createClient();
+  try {
+    await client.connect();
+    const sql = `INSERT INTO comment_TB (post_id, user_id, content) VALUES ($1, $2, $3)`;
+    const params = [postId, userId, content];
 
-  db.query(sql, param, (error, results, fields) => {
-    if (error) {
-      printError(error, result, res);
-      return;
-    }
-
-    const data = results.affectedRows === 0;
-    if (!data) {
+    const data = await client.query(sql, params);
+    if (data.rowCount !== 0) {
       result.success = true;
-      result.message = "댓글 작성 성공";
+      result.message = "댓글 작성에 성공하였습니다";
+    } else {
+      result.message = "댓글 작성에 실패하였습니다";
     }
 
+  } catch (error) {
+    result.message = "POST /api/comment/ error: " + error.message;
+
+  } finally {
+    await client.end();
     res.send(result);
-  });
+  }
 };
 
-const readComment = (req, res) => {
+const readComment = async (req, res) => {
   const { postId } = req.params;
   const result = makeResult();
 
@@ -45,33 +48,33 @@ const readComment = (req, res) => {
     return;
   }
 
-  // const sql = "SELECT * FROM comment_TB WHERE post_id = ?";
-  const sql = `SELECT comment_TB.id, comment_TB.user_id, comment_TB.content, comment_TB.created_date, comment_TB.updated_date, 
+  const client = createClient();
+  try {
+    await client.connect();
+    const sql = `SELECT comment_TB.id, comment_TB.user_id, comment_TB.content, comment_TB.created_date, comment_TB.updated_date, 
                user_TB.name AS author_name 
                FROM comment_TB JOIN user_TB ON comment_TB.user_id = user_TB.id 
-               WHERE post_id = ?`
-  const param = postId;
+               WHERE post_id = $1`
+    const params = [postId];
 
-  db.query(sql, param, (error, results, fields) => {
-    if (error) {
-      printError(error, result, res);
-      return;
-    }
-
-    const isEmptyResult = results.length === 0;
-    if (!isEmptyResult) {
+    const data = await client.query(sql, params);
+    if (data.rows.length !== 0) {
       result.success = true;
-      result.message = results;
-
+      result.message = data.rows;
     } else {
-      result.message = "게시글 또는 댓글이 존재하지 않습니다";
+      result.message = "해당 게시글에 댓글이 존재하지 않습니다";
     }
+    
+  } catch (error) {
+    result.message = "GET /api/comment/ error: " + error.message;
 
+  } finally {
+    await client.end();
     res.send(result);
-  });
-};
+  }
+}
 
-const updateComment = (req, res) => {
+const updateComment = async (req, res) => {
   const { userId, content, postId, commentId } = req.body;
   const result = makeResult();
 
@@ -82,30 +85,28 @@ const updateComment = (req, res) => {
     return;
   }
 
-  const sql = "UPDATE comment_TB SET content = ? WHERE post_id = ? AND user_id = ? AND id = ?";
-  const param = [content, postId, userId, commentId];
+  const client = createClient();
+  try {
+    await client.connect();
+    const sql = "UPDATE comment_TB SET content = $1 WHERE post_id = $2 AND user_id = $3 AND id = $4";
+    const param = [content, postId, userId, commentId];
 
-  db.query(sql, param, (error, results, fields) => {
-    if (error) {
-      printError(error, result, res);
-      return;
-    }
-
-    const data = results.affectedRows === 0;
-    if (!data) {
+    const data = await client.query(sql, param);
+    if (data.rowCount !== 0) {
       result.success = true;
-      result.message = "댓글 수정 성공";
-
+      result.message = "댓글 수정에 성공하였습니다";
     } else {
-      result.success = false;
-      result.message = "댓글 수정 실패 (본인 댓글이 아니거나 존재하지 않는 게시글임)";
+      result.message = "댓글 수정에 실패하였습니다";
     }
-
+  } catch (error) {
+    result.message = "GET /api/comment/ error: " + error.message;
+  } finally {
+    await client.end();
     res.send(result);
-  });
+  }
 };
 
-const deleteComment = (req, res) => {
+const deleteComment = async (req, res) => {
   const { postId, commentId, userId } = req.body;
   const result = makeResult();
 
@@ -116,67 +117,32 @@ const deleteComment = (req, res) => {
     return;
   }
 
-  const sql = "DELETE FROM comment_TB WHERE post_id = ? AND user_id = ? AND id = ?";
-  const param = [postId, userId, commentId];
+  const client = createClient();
+  try {
+    await client.connect();
+    const sql = "DELETE FROM comment_TB WHERE post_id = $1 AND user_id = $2 AND id = $3";
+    const params = [postId, userId, commentId];
 
-  db.query(sql, param, (error, results, fields) => {
-    if (error) {
-      printError(error, result, res);
-      return;
-    }
-
-    const data = results.affectedRows === 0;
-    if (!data) {
+    const data = await client.query(sql, params)
+    if (data.rowCount !== 0) {
       result.success = true;
       result.message = "댓글 삭제 성공";
-
     } else {
-      result.success = false;
-      result.message = "댓글 삭제 실패 (존재하지 않는 게시글, 댓글이거나 본인 댓글이 아님)";
+      result.message = "댓글 삭제 실패"
     }
+  } catch (error) {
+    result.message = "GET /api/comment/ error: " + error.message;
 
+  } finally {
+    await client.end();
     res.send(result);
-  });
-};
-
-const readUserComment = (req, res) => {
-  const { userLoginId } = req.params;
-  const result = makeResult();
-
-  const isValidateValue = commentValidate.validateReadUserCommentInput(userLoginId);
-  if (!isValidateValue) {
-    result.message = validateMessage;
-    res.send(result);
-    return;
   }
-
-  const sql = "SELECT * FROM comment_TB WHERE user_id IN (SELECT id FROM user_TB WHERE login_id = ?)";
-  const param = [userLoginId];
-
-  db.query(sql, param, (error, results, fields) => {
-    if (error) {
-      printError(error, result, res);
-      return;
-    }
-
-    // 게시글을 찾으면 true, 찾지 못하면 false
-    const isFindPost = results.length === 0;
-    if (!isFindPost) {
-      result.success = true;
-      result.message = results;
-
-    } else {
-      result.message = "해당하는 사용자의 게시글이 존재하지 않습니다";
-    }
-
-    res.send(result);
-  });
-}
+};
 
 module.exports = {
   createComment,
   readComment,
   updateComment,
   deleteComment,
-  readUserComment
+  // readUserComment
 }
