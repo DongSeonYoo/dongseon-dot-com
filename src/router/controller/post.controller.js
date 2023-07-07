e235ceb88125df036da2d716824e29b03668ceb6
ehdtjs0612 <inko51366@naver.com>
2023-07-07 06:40:15 +0000

    refactor: migration db post api(createPost, readAllPost) (mariadb -> postgresql)

--- src/router/controller/post.controller.js
+++ src/router/controller/post.controller.js
@@ -76,30 +76,33 @@ const readPost = (req, res) => {
     return res.send(result);
   }
 
-  const sql = `SELECT post_TB.*, 
-               user_TB.name AS author_name 
-               FROM post_TB 
-               JOIN user_TB ON post_TB.user_id = user_TB.id 
-               WHERE post_TB.id = ?`;
-  const param = postId;
+  const client = createClient();
+  client.connect()
+    .then(() => {
+      const sql = `SELECT post_TB.*, 
+      user_TB.name AS author_name 
+      FROM post_TB 
+      JOIN user_TB ON post_TB.user_id = user_TB.id 
+      WHERE post_TB.id = $1`;
+      const params = [postId];
 
-  db.query(sql, param, (error, results, fields) => {
-    if (error) {
-      printError(error, result, res);
-      return;
-    }
-
-    // 게시글을 찾으면 true, 찾지 못하면 false
-    const isFindPost = results.length === 0;
-    if (!isFindPost) {
-      result.success = true;
-      result.message = results[0];
-    } else {
-      result.message = "해당하는 게시글이 존재하지 않습니다";
-    }
-
-    res.send(result);
-  });
+      return client.query(sql, params);
+    })
+    .then((data) => {
+      if (data.rows.length !== 0) {
+        result.success = true;
+        result.message = data.rows[0];
+      } else {
+        result.message = "해당하는 게시글이 존재하지 않습니다";
+      }
+    })
+    .catch((error) => {
+      result.message = "GET /api/post/:userId/ error: " + error.message;
+    })
+    .finally(() => {
+      client.end();
+      res.send(result);
+    });
 };
 
 const updatePost = (req, res) => {
@@ -113,26 +116,27 @@ const updatePost = (req, res) => {
     return;
   }
 
-  const sql = "UPDATE post_TB SET title = ?, content = ? WHERE user_id = ? AND id = ?";
-  const param = [title, content, userId, postId];
+  const client = createClient();
+  client.connect()
+    .then(() => {
+      const sql = "UPDATE post_TB SET title = $1, content = $2 WHERE user_id = $3 AND id = $4";
+      const params = [title, content, userId, postId];
 
-  db.query(sql, param, (error, results, fields) => {
-    if (error) {
-      printError(error, result, res);
-      return;
-    }
-
-    const data = results.affectedRows === 0;
-    if (!data) {
-      result.success = true;
-      result.message = "수정 성공";
-
-    } else {
-      result.message = "수정 실패, 본인만 수정 가능";
-    }
-
-    res.send(result);
-  });
+      return client.query(sql, params);
+    })
+    .then((data) => {
+      if (data.rowCount !== 0) {
+        result.success = true;
+        result.message = "수정에 성공하였습니다";
+      }
+    })
+    .catch((error) => {
+      result.message = "PUT /api/post/:userId/ error: " + error.message;
+    })
+    .finally(() => {
+      client.end();
+      res.send(result);
+    })
 };
 
 const deletePost = (req, res) => {
@@ -146,61 +150,64 @@ const deletePost = (req, res) => {
     return;
   }
 
-  const sql = "DELETE FROM post_TB WHERE id = ? AND user_id = ?";
-  const param = [postId, userId];
+  const client = createClient();
+  client.connect()
+    .then(() => {
+      const sql = "DELETE FROM post_TB WHERE id = $1 AND user_id = $2";
+      const params = [postId, userId];
 
-  db.query(sql, param, (error, results, fields) => {
-    if (error) {
-      printError(error, result, res);
-      return;
-    }
-
-    const isDeleted = results.affectedRows === 0;
-    if (!isDeleted) {
-      result.success = true;
-      result.message = "삭제 성공";
-
-    } else {
-      result.message = "삭제 실패, 게시글이 존재하지 않거나 본인만 삭제 가능합니다";
-    }
-
-    res.send(result);
-  });
+      return client.query(sql, params);
+    })
+    .then((data) => {
+      if (data.rowCount !== 0) {
+        result.success = true;
+        result.message = "삭제에 성공하였습니다";
+      } else {
+        result.message = "삭제에 실패하였습니다(존재하지 않는 사용자 혹은 게시물)";
+      }
+    })
+    .catch((error) => {
+      result.message = "DELETE /api/post/ error: " + error.message;
+    })
+    .finally(() => {
+      client.end();
+      res.send(result);
+    });
 };
 
-const getUserPost = (req, res) => {
-  const { userLoginId } = req.params;
-  const result = makeResult();
+// const getUserPost = (req, res) => {
+//   const { userLoginId } = req.params;
+//   const result = makeResult();
 
-  const isValidateInput = postValidate.validateGetUserPostInput(userLoginId);
-  if (!isValidateInput) {
-    result.message = validateMessage;
-    res.send(result);
-    return;
-  }
+//   const isValidateInput = postValidate.validateGetUserPostInput(userLoginId);
+//   if (!isValidateInput) {
+//     result.message = validateMessage;
+//     res.send(result);
+//     return;
+//   }
 
-  const sql = "SELECT * FROM post_TB WHERE user_id IN (SELECT id FROM user_TB WHERE login_id = ?)";
-  const param = [userLoginId];
+//   const sql = "SELECT * FROM post_TB WHERE user_id IN (SELECT id FROM user_TB WHERE login_id = ?)";
+//   const param = [userLoginId];
 
-  db.query(sql, param, (error, results, fields) => {
-    if (error) {
-      printError(error, result, res);
-      return;
-    }
+//   db.query(sql, param, (error, results, fields) => {
+//     if (error) {
+//       printError(error, result, res);
+//       return;
+//     }
 
-    // 게시글을 찾으면 true, 찾지 못하면 false
-    const isFindPost = results.length === 0;
-    if (!isFindPost) {
-      result.success = true;
-      result.message = results;
+//     // 게시글을 찾으면 true, 찾지 못하면 false
+//     const isFindPost = results.length === 0;
+//     if (!isFindPost) {
+//       result.success = true;
+//       result.message = results;
 
-    } else {
-      result.message = "해당하는 사용자의 게시글이 존재하지 않습니다";
-    }
+//     } else {
+//       result.message = "해당하는 사용자의 게시글이 존재하지 않습니다";
+//     }
 
-    res.send(result);
-  });
-};
+//     res.send(result);
+//   });
+// };
 
 module.exports = {
   createPost,
@@ -208,5 +215,4 @@ module.exports = {
   readPost,
   updatePost,
   deletePost,
-  getUserPost
 }
