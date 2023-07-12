2ab56cceb965f3f4ca66a520b1e96939fa41c175
ehdtjs0612 <inko51366@naver.com>
2023-07-12 07:34:25 +0000

    feat: 전역변수를 관리하는 모듈 생성

---
+++ src/module/global.js
@@ -0,0 +1,18 @@
+module.exports = {
+  //account
+  maxUserIdLength: 8,
+  maxLoginIdLength: 20,
+  maxPwLength: 15,
+  maxNameLength: 8,
+  maxPhoneNumberLength: 11,
+  maxEmailLength: 320,
+
+  //post
+  maxPostIdLength: 10,
+  maxPostTitleLength: 30,
+  maxPostContentLength: 500,
+
+  //comment
+  maxCommentIdLength: 8,
+  maxCommentContentLength: 300
+}
