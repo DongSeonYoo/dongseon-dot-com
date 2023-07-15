52a85db01d3dd6ab8fedfe953e671db52d549508
ehdtjs0612 <inko51366@naver.com>
2023-07-15 14:54:21 +0000

    feat: res.send() 메서드 오버라이딩을 통해 logging에 응답 객체 전송

--- server.js
+++ server.js
@@ -1,49 +1,29 @@
 const express = require("express");
 const app = express();
-
-const morgan = require("morgan");
-const client = require("mongodb").MongoClient;
+const logging = require("./src/module/logging");
 
 const pagesRoute = require("./src/router/pages");
 const accountApi = require("./src/router/account");
 const postApi = require("./src/router/post");
 const commentApi = require("./src/router/comment");
 
+require("dotenv").config();
+
 app.use(express.json());
 app.use(express.static("client/"));
-
-app.use(async (req, res, next) => {
-  let connect = null;
-
-  try {
-    connect = await client.connect("mongodb://localhost:27017");
-    const document = {
-      "ip": req.ip,
-      "time": new Date().toISOString(),
-      "method": req.method,
-      "path": req.path,
-    }
-
-    await connect.db("logs").collection("api_logs").insertOne(document);
-    
-  } catch (error) {
-    console.log("logs.api_logs Error: " + error.message);
-
-  } finally {
-    if (connect) {
-      await connect.close();
-    }
-    next();
-  }
-})
-
 app.use((req, res, next) => {
-  console.log(req);
-  console.log(res);
-  next();
-})
-app.use("/", pagesRoute);
+  const originResultSend = res.send;
 
+  res.send = function(result) {
+    if(typeof(result) !== 'string'){
+      logging(req, res, result);
+    }
+    return originResultSend.call(this, result);
+  }
+  next();
+});
+
+app.use("/", pagesRoute);
 app.use("/api/account", accountApi);
 app.use("/api/post", postApi);
 app.use("/api/comment", commentApi);
