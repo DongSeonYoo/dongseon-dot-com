750851c7f2b631a66fd2c8e724c69b621ac5e4d7
ehdtjs0612 <inko51366@naver.com>
2023-07-16 14:04:37 +0000

    feat: log에 moment 라이브러리를 이용하여 시간을 담아줌

--- src/module/logging.js
+++ src/module/logging.js
@@ -1,26 +1,23 @@
 const mongoClient = require("mongodb").MongoClient;
+const moment = require("moment");
+const momentTimeZone = require("moment-timezone");
+
 require("dotenv").config();
+// 한국시간 설정
+momentTimeZone.tz("Asia/Seoul");
 
 const logging = async (req, res, resBody) => {
-  // const reqObj = {
-  //   type: "request",
-  //   time: new Date(),
-  //   body: req.body
-  // }
-  // const resObj = {
-  //   type: "response",
-  //   time: new Date(),
-  //   body: resBody
-  // }
   const document = {
+    time: moment().format("YYYY-MM-DD HH:mm:ss"),
     ip: req.ip,
+    status: res.statusCode,
     method: req.method,
     api: req.originalUrl,
     req: req.body,
     res: resBody
   }
-  let connect = null;
   
+  let connect = null;
   try {
     connect = await mongoClient.connect(process.env.MONGO_DB_LOGS);
     await connect.db().collection("api_logs").insertOne(document);
