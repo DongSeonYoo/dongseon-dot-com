59e60e74c2c4685dd8c1e80ba1c2213e8fcddf4e
ehdtjs0612 <inko51366@naver.com>
2023-07-21 12:52:47 +0000

    fix: app.listen 부분에 서버를 0.0.0.0으로 open

--- bin/www.js
+++ bin/www.js
@@ -2,6 +2,6 @@ const app = require("../server");
 require("dotenv").config();
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log("8000 포트 켜짐");
 });
