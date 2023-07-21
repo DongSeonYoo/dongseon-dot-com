d02d7f48a9a0aa2c78f1a34d71d332dc59340839
ehdtjs0612 <inko51366@naver.com>
2023-07-21 12:54:32 +0000

    feat: logging 세팅 부분을 분리

--- server.js
+++ server.js
@@ -2,30 +2,20 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const CLIENT_PATH = path.join(__dirname,'./client/pages');
-const logging = require("./src/module/logging");
 
 const pagesRoute = require("./src/router/pages");
 const accountApi = require("./src/router/account");
 const postApi = require("./src/router/post");
 const commentApi = require("./src/router/comment");
 const logApi = require("./src/router/log");
+const loggingSetting = require('./src/middleware/logginSetting');
+
 require("dotenv").config();
+// 구간별로 나누자
 
 app.use(express.json());
 app.use(express.static("client/"));
-app.use((req, res, next) => {
-  const originResultSend = res.send;
-
-  res.send = function(result) {
-    // if(typeof result !== 'string'){} ?
-    if(typeof result !== 'string' && req.originalUrl.split('/')[2] !== "log") {
-      logging(req, res, result);
-    }
-
-    return originResultSend.call(this, result);
-  }
-  next();
-});
+app.use(loggingSetting());
 
 app.use("/", pagesRoute);
 app.use("/api/account", accountApi);
@@ -38,8 +28,10 @@ app.use((req, res, next) => {
   res.status(404).sendFile(path.join(CLIENT_PATH, '404.html'))
 });
 
-// 500 error catch middleware
+// error catch middleware
 app.use((err, req, res, next) => {
+  req.err = err;
+
   res.status(500);
 });
 
