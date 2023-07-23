f05c638b08a12f265de3470567afc2ed7eddf3d1
ehdtjs0612 <inko51366@naver.com>
2023-07-23 14:58:39 +0000

    refactor: 에러 인스턴스를 던지게 수정

--- src/module/exception.js
+++ src/module/exception.js
@@ -3,81 +3,81 @@ const { loginIdRegex, pwRegex, nameRegex, phoneNumberRegex, emailRegex } = requi
 function Exception(input, name) {
   this.checkInput = () => {
     if (input === undefined) {
-      const error = Error(`(${name}): 요청값이 잘못되었습니다`);
+      const error = new Error(`(${name}): 요청값이 잘못되었습니다`);
       error.status = 400;
-      
       throw error;
     }
+
     return this;
   }
 
   this.checkLength = (min, max) => {
     if (input.length < min || input.length > max) {
-      const error = Error(`(${name}): 길이가 비정상적입니다`);
+      const error = new Error(`(${name}): 길이가 비정상적입니다`);
       error.status = 400;
-
       throw error;
     }
+
     return this;
   }
 
   this.checkIdRegex = () => {
     if (!loginIdRegex.test(input)) {
-      const error = Error(`(${name}): 정규표현식 실패`);
+      const error = new Error(`(${name}): 정규표현식 실패`);
       error.status = 400;
-
       throw error;
     }
+
     return this;
   }
 
   this.checkPwRegex = () => {
     if (!pwRegex.test(input)) {
-      const error = Error(`(${name}): 정규표현식 실패`);
+      const error = new Error(`(${name}): 정규표현식 실패`);
       error.status = 400;
-
       throw error;
     }
+
     return this;
   }
 
   this.checkNameRegex = () => {
     if (!nameRegex.test(input)) {
-      const error = Error(`(${name}): 정규표현식 실패`);
+      const error = new Error(`(${name}): 정규표현식 실패`);
       error.status = 400;
-
       throw error;
     }
+
     return this;
   }
 
   this.checkPhoneNumberRegex = () => {
     if (!phoneNumberRegex.test(input)) { 
-      const error = Error(`(${name}): 정규식표현식 실패`);
+      const error = new Error(`(${name}): 정규식표현식 실패`);
       error.status = 400;
-
       throw error;
     }
+
     return this;
   }
 
   this.checkEmailRegex = () => {
     if (!emailRegex.test(input)) {
-      const error = Error(`(${name}): 정규표현식 실패`);
+      const error = new Error(`(${name}): 정규표현식 실패`);
       error.status = 400;
-
       throw error;
     }
+
     return this;
   }
 
   this.isNumber = () => {
     if (isNaN(Number(input))) {
-      const error = Error(`(${name}): 정수가 아닙니다`);
+      const error = new Error(`(${name}): 정수가 아닙니다`);
       error.status = 400;
-
       throw error;
     }
+    
     return this;
   }
 }
