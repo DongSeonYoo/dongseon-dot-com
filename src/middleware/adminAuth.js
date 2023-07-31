9ff58b0ceed50eef912481848aa928d870790f77
ehdtjs0612 <inko51366@naver.com>
2023-07-31 07:32:16 +0000

    feat: admin권한 조회하는 라우터 추가

---
+++ src/middleware/adminAuth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = async (req, res, next) => {
+  // 쿠키에 담긴 토큰을 추출
+  const { accessToken } = req.cookies;
+  try {
+    // 쿠키 이름이 잘못되었을때?(쿠키 이름을 조작한경우)
+    if (!accessToken) {
+      throw new Error("invalid token");
+    }
+    req.decoded = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
+    const { role } = req.decoded;
+    if (role !== "admin") {
+      throw new Error("admin only")
+    }
+    next();
+
+  } catch (error) {
+    if (error.message === "jwt expired") {
+      error.status = 419;
+    } else if (error.message === "invalid token") {
+      error.status = 401;
+    } else if (error.message === "admin only") {
+      error.status = 403;
+    }
+    next(error);
+  }
+};
--- src/router/auth.js
+++ src/router/auth.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const loginAuth = require("../middleware/loginAuth")
+const loginAuth = require("../middleware/loginAuth");
+const adminAuth = require('../middleware/adminAuth');
 
-router.get("/",loginAuth(), (req, res) => {
+router.get("/login", loginAuth, (req, res) => {
   res.status(200).send({
     data: req.decoded,
-  })
+  });
+});
+
+router.get("/admin", adminAuth, (req, res) => {
+  res.status(200).send({
+    data: req.decoded,
+  });
 });
 
 module.exports = router;
