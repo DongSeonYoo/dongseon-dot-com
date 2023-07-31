7ba4fa7f576fd6ebe43761791cff1a2a56fee653
ehdtjs0612 <inko51366@naver.com>
2023-07-31 12:43:06 +0000

    fix: jwt sign 옵션 수정

--- src/module/jwt.js
+++ src/module/jwt.js
@@ -1,12 +1,13 @@
-const { secretKey, options } = require("../../config/secretKey");
+const { secretKey, option } = require("../../config/secretKey");
 const jwt = require("jsonwebtoken");
 
-const userSign = async (userPk) => {
+const userSign = async (userPk, loginId) => {
   const payload = {
     userPk,
+    loginId,
     role: "user",
   }
-  return jwt.sign(payload, secretKey, options);
+  return jwt.sign(payload, secretKey, option);
 }
 
 const adminSign = async () => {
@@ -14,7 +15,7 @@ const adminSign = async () => {
     userPk: "",
     role: "admin",
   }
-  return jwt.sign(payload, secretKey, options);
+  return jwt.sign(payload, secretKey, option);
 }
 
 module.exports = {
