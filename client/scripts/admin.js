eeae8ff28e17238bbb78c7334f8f739034c07348
ehdtjs0612 <inko51366@naver.com>
2023-08-01 10:03:38 +0000

    feat: add admin page

--- client/scripts/admin.js
+++ client/scripts/admin.js
@@ -4,22 +4,28 @@ const oldOption = document.getElementById("old-option");
 const prevPageBtn = document.getElementById("prev-page-button");
 const nextPageBtn = document.getElementById("next-page-button");
 const logCountDiv = document.getElementById("log-count");
+const idSerchForm = document.getElementById("loginid-search-form");
 
 let apiCounts;
 let maxPageCount;
 let selectedOrderValue = "recent";
 let selectedMethodValue = "all";
+let selectedSerchValue = "";
 let currentPage = 1;
+
+const data = {
+  req: "",
+  res: "",
+};
 
 window.onload = async () => {
   // 관리자 권한 체크
   try {
-    await adminAuthCheck();
+    // await adminAuthCheck();
     const logsCount = await getDocumentCountFetch();
     apiCounts = logsCount;
     maxPageCount = Math.ceil(logsCount / 16);
-    updatePageMoveButton();
-    logCountDiv.innerHTML = logsCount + "개의 로그";
+    
 
     await loadApiFetch(selectedOrderValue, selectedMethodValue, currentPage);
   } catch (error) {
@@ -29,9 +35,7 @@ window.onload = async () => {
 
 const onchangePage = (page) => {
   currentPage = page;
-  logSection.innerHTML = "";
   loadApiFetch(selectedOrderValue, selectedMethodValue, currentPage);
-  updatePageMoveButton();
 }
 
 const updatePageMoveButton = () => {
@@ -57,17 +61,26 @@ const getDocumentCountFetch = async () => {
   }
 }
 
-const loadApiFetch = async (order, method, page) => {
+const loadApiFetch = async (order, method, page, loginId = selectedSerchValue) => {
   try {
-    const response = await fetch(`/api/log?order=${order}&method=${method}&page=${page}`);
+    const response = await fetch(`/api/log?order=${order}&method=${method}&page=${page}&loginId=${loginId}`);
     const json = await response.json();
 
     if (response.status === 200) {
       if (json.data) {
+        logSection.innerHTML = "";
         const responsedData = json.data;
         responsedData.forEach(data => makeTag(data));
       }
+
+      if (json.logCount) {
+        apiCounts = json.logCount;
+        maxPageCount = Math.ceil(apiCounts / 16);
+        logCountDiv.innerHTML = apiCounts + "개의 로그";
+        console.log(json.logCount);
+      }
     }
+    updatePageMoveButton();
 
   } catch (error) {
     console.error(error);
@@ -78,6 +91,7 @@ const loadApiFetch = async (order, method, page) => {
 const makeTag = (data) => {
   const logDiv = document.createElement("div");
   logDiv.classList.add("log");
+  logDiv.onclick = clickLog;
 
   const timeSpan = document.createElement("span");
   timeSpan.classList.add("time");
@@ -95,6 +109,14 @@ const makeTag = (data) => {
   methodSpan.classList.add("method");
   methodSpan.textContent = data.method;
 
+  const loginIdSpan = document.createElement("span");
+  loginIdSpan.classList.add("loginId");
+  if (data.loginId) {
+    loginIdSpan.textContent = data.loginId;
+  } else {
+    loginIdSpan.textContent = "로그인 X";
+  }
+
   const apiSpan = document.createElement("span");
   apiSpan.classList.add("api");
   apiSpan.textContent = truncateData(JSON.stringify(data.api));
@@ -110,15 +132,30 @@ const makeTag = (data) => {
   resSpan.classList.add("response");
   resSpan.textContent = truncateData(JSON.stringify(data.res));
 
+  const reqValue = document.createElement("input");
+  reqValue.value = JSON.stringify(data.req);
+  reqValue.type = 'hidden';
+
+  const resValue = document.createElement("input");
+  resValue.value = JSON.stringify(data.res);
+  resValue.type = 'hidden';
+
   logDiv.appendChild(timeSpan);
   logDiv.appendChild(ipSpan);
   logDiv.appendChild(statusSpan);
   logDiv.appendChild(methodSpan);
+  logDiv.appendChild(loginIdSpan);
   logDiv.appendChild(apiSpan);
   logDiv.appendChild(reqSpan);
   logDiv.appendChild(resSpan);
+  logDiv.appendChild(reqValue);
+  logDiv.appendChild(resValue);
 
   logSection.appendChild(logDiv);
+}
+
+const clickLog = (e) => {
+  console.log(e.target)
 }
 
 // 요청과 응답 데이터가 너무 길 경우 줄여서 표시
@@ -132,23 +169,17 @@ const truncateData = (data) => {
 
 const onchangeOrderOption = () => {
   const orderOptionList = document.getElementById("order-option-list");
+
   selectedOrderValue = orderOptionList[orderOptionList.selectedIndex].value;
-
   currentPage = 1;
-  updatePageMoveButton();
-
-  logSection.innerHTML = "";
   loadApiFetch(selectedOrderValue, selectedMethodValue, currentPage);
 }
 
 const onchangeMethodOption = () => {
   const methodOptionList = document.getElementById("method-option-list");
+
   selectedMethodValue = methodOptionList[methodOptionList.selectedIndex].value;
-
   currentPage = 1;
-  updatePageMoveButton();
-
-  logSection.innerHTML = "";
   loadApiFetch(selectedOrderValue, selectedMethodValue, currentPage);
 }
 
@@ -161,5 +192,13 @@ const onclickPrevButton = () => {
 const onclickNextButton = () => {
   if (currentPage < maxPageCount) {
     onchangePage(currentPage + 1);
+  }
+}
+
+const onclickSerchButton = () => {
+  selectedSerchValue = idSerchForm.value;
+  if (selectedSerchValue) {
+    console.log(selectedSerchValue);
+    loadApiFetch(selectedOrderValue, selectedMethodValue, currentPage, selectedSerchValue);
   }
 }
