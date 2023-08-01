98e53e36e552f8a6960d035bb3ace861e687a90a
ehdtjs0612 <inko51366@naver.com>
2023-08-01 10:06:25 +0000

    feat: userAuth 권한 체크 시 토큰의 이름이 유효한지 검사하는 로직 추가

--- src/middleware/loginAuth.js
+++ src/middleware/loginAuth.js
@@ -1,13 +1,17 @@
 const jwt = require("jsonwebtoken");
 const createClient = require("../../config/database/postgresql");
-
 require("dotenv").config();
 
 module.exports = async (req, res, next) => {
   let client = null;
-  // 쿠키에 담긴 토큰을 가져온다음
+  // 쿠키에 담긴 토큰을 추출
   const accessToken = req.cookies.accessToken;
   try {
+    // 쿠키 이름이 잘못되었을때?(쿠키 이름을 조작한경우)
+    if (!accessToken) {
+      throw new Error("invalid token");
+    }
+
     // 토큰이 블랙리스트에 등록되어있는지 체크
     client = createClient();
     await client.connect();
@@ -31,11 +35,10 @@ module.exports = async (req, res, next) => {
   } catch (error) {
     if (error.message === "jwt expired") {
       error.status = 419;
-      next(error);
-    } else {
+    } else if (error.message === "invalid token") {
       error.status = 401;
-      next(error);
     }
+    next(error);
   } finally {
     if (client) await client.end();
   }
