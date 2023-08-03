bdbe0600e7a3d57cedd27aa3fb8978223e7297f0
ehdtjs0612 <inko51366@naver.com>
2023-08-03 14:57:59 +0000

    feat: convert linux time

--- src/router/auth.js
+++ src/router/auth.js
@@ -4,12 +4,14 @@ const loginAuth = require("../middleware/loginAuth");
 const adminAuth = require('../middleware/adminAuth');
 
 router.get("/login", loginAuth, (req, res) => {
+    req.decoded.iat = new Date(req.decoded.iat * 1000);
     res.status(200).send({
         data: req.decoded,
     });
 });
 
 router.get("/admin", adminAuth, (req, res) => {
+    req.decoded.iat = new Date(req.decoded.iat * 1000);
     res.status(200).send({
         data: req.decoded,
     });
