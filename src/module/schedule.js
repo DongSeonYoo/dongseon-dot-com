9e0335c64967929ae43cc1e34d1bfe28dbfed150
ehdtjs0612 <inko51366@naver.com>
2023-08-05 08:04:55 +0000

    feat: 1시간 간격으로(매 0분마다) redis에 저장된 데이터를 postgresql에 기록

--- src/module/schedule.js
+++ src/module/schedule.js
@@ -1,10 +1,48 @@
 const schedule = require("node-schedule");
 const rule = new schedule.RecurrenceRule();
-rule.second = 0;
 
+const redisClient = require("redis").createClient();
+const createClient = require("../../config/database/postgresql");
+
+//매 0분마다 (한시간마다) 실행
+rule.minute = 0;
 const job = schedule.scheduleJob(rule, () => {
-    // redis의 데이터를 postgresql로 옮겨주자
-    
-})
+    moveDataToPostgres();
+});
 
-module.exports = job
+
+// redis -> postgresql
+const moveDataToPostgres = async () => {
+    let pgClient = null;
+
+    try {
+        pgClient = createClient();
+        // redis, postgresql 데이터베이스 연결
+        await redisClient.connect();
+        await pgClient.connect();
+
+        // redis에 저장되어있는 1시간동안의 로그인 기록을 가져옴 (type: array)
+        const data = await redisClient.sMembers("dailyLoginUser");
+
+        // postgresql에 데이터 삽입
+        for (const loginId of data) {
+            const query = `INSERT INTO logged_in_user (login_id, created_date, updated_date) VALUES ($1, $2, $3)`;
+            const values = [loginId, new Date(), new Date()];
+
+            await pgClient.query(query, values);
+        }
+        await redisClient.del("dailyLoginUser");
+
+        console.log("스케쥴러 작동 성공");
+
+    } catch (error) {
+        console.error(error);
+
+    } finally {
+        // redis, postgresql 연결 해제
+        await redisClient.disconnect();
+        await pgClient.end();
+    }
+}
+
+module.exports = job;
