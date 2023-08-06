1ff8be799ec06e39008b0e94775c47cf6fa7f396
ehdtjs0612 <inko51366@naver.com>
2023-08-06 14:59:05 +0000

    refactor: 로그 보내주는 api의 응답형식을 변경 (result.data.log, result.data.logCount)

--- src/router/log.js
+++ src/router/log.js
@@ -1,17 +1,20 @@
 const router = require("express").Router();
 const mongoClient = require("mongodb").MongoClient;
+const redisClient = require("redis").createClient();
+
 const exception = require("../module/exception");
 const { maxItemPerPage } = require("../module/global");
 const adminAuth = require("../middleware/adminAuth");
 const { maxLoginIdLength } = require("../module/global");
 require("dotenv").config();
 
+// 한 페이지의 로그와 전체 로그의 개수를 보내주는 api
 router.get("/", adminAuth, async (req, res, next) => {
     let { order, method, page, loginId = "" } = req.query;
     let queryOption;
     const result = {
-        data: "",
-    }
+        data: {},
+    };
 
     let connect = null;
     try {
@@ -41,27 +44,28 @@ router.get("/", adminAuth, async (req, res, next) => {
 
         // db연결
         connect = await mongoClient.connect(process.env.MONGO_DB_LOGS);
-        // 쿼리 실행
-        const cursor = await connect
+
+        // mongodb 쿼리 실행
+        const logData = await connect
             .db()
             .collection("api_logs")
             .find(queryOption)
             .sort({ "_id": order })
             .skip((page - 1) * maxItemPerPage)
-            .limit(maxItemPerPage);
-
-        const data = await cursor.toArray();
-        result.data = data;
-
+            .limit(maxItemPerPage)
+            .toArray();
         const logCount = await connect
             .db()
             .collection("api_logs")
             .countDocuments(queryOption);
 
-        result.logCount = logCount;
+        // 결과 담아주자
+        result.data.log = logData;
+        result.data.logCount = logCount;
         res.send(result);
 
     } catch (error) {
+        console.error(error);
         next(error);
 
     } finally {
