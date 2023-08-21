b209fd642422e0800dbf85950e344004c95951e2
ehdtjs0612 <inko51366@naver.com>
2023-08-21 14:33:45 +0000

    feat: set secret key of refresh token

--- config/secretKey.js
+++ config/secretKey.js
@@ -2,9 +2,14 @@ require("dotenv").config();
 
 module.exports = {
     secretKey: process.env.JWT_SECRET_KEY,
-    option: {
+    accessTokenOption: {
         "algorithm": "HS256",
         "expiresIn": "1h",
+        "issuer": "ehdtjs.com"
+    },
+    refreshTokenOption: {
+        "algorithm": "HS256",
+        "expiresIn": "14d",
         "issuer": "ehdtjs.com"
     }
 }
