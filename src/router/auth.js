67ac9a2f39bfaa869a0bf9db53b3c6d9c71769a3
ehdtjs0612 <inko51366@naver.com>
2023-09-04 06:42:53 +0000

    feat: kakao에서 받아온 회원 정보를 이용해 새로운 회원은 회원가입, 기존 유저는 로그인 시켜줌

--- src/router/auth.js
+++ src/router/auth.js
@@ -12,29 +12,28 @@ passport.use('kakao', new KakaoStrategy({
     clientID: process.env.KAKAO_ID,
     callbackURL: process.env.KAKAO_CALLBACKURL,
 }, async (accessToken, refreshToken, profile, done) => {
-    // 인증 전략 실행하자
-    // 기존 테이블을 새로 만들어서 관리할건지 기존 테이블에 컬럼을 추가해서 통합으로 관리할건지
-    console.log(`accessTOken: ${accessToken}`);
-    console.log(`refreshToken: ${refreshToken}`);
-
+    // 인증 전략 실행
     try {
         const { id, username, provider } = profile;
         const email = profile._json.kakao_account.email;
 
-        const selectSql = `SELECT id FROM user_TB WHERE login_id = $1`;
+        const selectSql = `SELECT id, login_id, name, provider FROM user_TB WHERE login_id = $1`;
         const params = [id];
 
         const selectedData = await pool.query(selectSql, params);
         // 만약 기존에 가입한 유저가 아니라면 (회원가입시켜줘야지)
         if (selectedData.rows.length === 0) {
-            const insertSql = "INSERT INTO user_TB (login_id, name, email, provider) VALUES ($1, $2, $3, $4)";
+            const insertSql = "INSERT INTO user_TB (login_id, name, email, provider) VALUES ($1, $2, $3, $4) RETURNING id";
             const params = [id, username, email, provider];
-            await pool.query(insertSql, params);
+            const insertedData = await pool.query(insertSql, params);
+            return done(null, insertedData);
         }
+        // 만약 기존에 가입한 유저라면 (바로 로그인 시켜줘야지)
         done(null, selectedData);
 
     } catch (error) {
         console.log(error);
+        done(error);
     }
 }));
 
