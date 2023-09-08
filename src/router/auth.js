5fdd84eaf8c6e824919a7887bf531ba65f075771
ehdtjs0612 <inko51366@naver.com>
2023-09-08 14:59:50 +0000

    feat: 인증번호 관련 모듈 생성

--- src/router/auth.js
+++ src/router/auth.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const authGuard = require("../middleware/authGuard");
 const adminAuth = require('../middleware/adminAuth');
 const passport = require("passport");
+const exception = require("../module/exception");
+const emailHandler = require("../module/mail");
+const redisClient = require("../../config/database/redis");
 
 const jwtUtil = require("../module/jwt");
 require("dotenv").config();
@@ -22,6 +25,45 @@ router.get('/kakao/callback', passport.authenticate("kakao", {
         secure: false,
     });
     res.redirect("/");
+});
+
+// 인증번호 전송 api
+router.post("/send-auth-email", (req, res, next) => {
+    const { email } = req.body;
+    const result = {
+        message: ""
+    }
+    try {
+        exception(email, "email").checkInput().checkEmailRegex();
+        emailHandler.sendVerifyEmail(email);
+    } catch (error) {
+        next(error);
+    }
+    result.message = "이메일 전송 완료";
+
+    res.send(result);
+});
+
+// 인증번호 체크
+router.post("/check-auth-email", async (req, res, next) => {
+    const { email, code } = req.body;
+
+    try {
+        exception(email, "email").checkInput().checkEmailRegex();
+        exception(code, "code").checkInput().isNumber();
+
+        const data = await redisClient.get(email, code);
+        if (data) {
+            // 인증번호가 유효한 경우
+
+        } else {
+            // 인증번호가 유효하지 않은 경우
+
+        }
+
+    } catch (error) {
+        next(error);
+    }
 });
 
 // 로그인 권한 체크 api
