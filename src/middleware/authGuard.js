cd19b20a376db1bc7546929de013382da5bf1c2f
ehdtjs0612 <inko51366@naver.com>
2023-09-23 05:55:54 +0000

    refactor: use custom error in verify jsonwebtoken

--- src/middleware/authGuard.js
+++ src/middleware/authGuard.js
@@ -8,18 +8,10 @@ module.exports = (req, res, next) => {
     const { accessToken } = req.cookies;
 
     try {
-        // 쿠키 이름이 잘못되었을때?(쿠키 이름을 조작한경우)
-        if (!accessToken) {
-            throw new Error("invalid token");
-        }
-
         req.decoded = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
         return next();
 
     } catch (error) {
-        if (error.message === "jwt expired" || error.message === "invalid token") {
-            return next(new UnauthorizedException("로그인 후 이용가능합니다"));
-        }
-        next(error);
+        return next(new UnauthorizedException("로그인 후 이용가능합니다"));
     }
 };
