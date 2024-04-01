f743e8a01f3d751ecfa0154ff11e721b3c7e3a1d
ehdtjs0612 <inko51366@naver.com>
2024-04-01 20:06:03 +0900

    feat: init swagger & init global api prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+
+  // init swagger
+  const config = new DocumentBuilder()
+    .setTitle('dongseon.com')
+    .setDescription('동선닷컴')
+    .setVersion('2.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
