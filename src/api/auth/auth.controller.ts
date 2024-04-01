d75235abe5a8ca6b6038c5cf476abc8fa848a6f6
ehdtjs0612 <inko51366@naver.com>
2024-04-01 20:05:14 +0900

    feat: add POST /api/signup api

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { SignupRequestDto, SignupResponseDto } from './dto/signup.dto';
+import { ResponseEntity } from 'src/common/util/common-response.util';
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/signup')
+  @HttpCode(HttpStatus.OK)
+  @ApiBody({ type: SignupRequestDto })
+  @ApiResponse({ type: SignupResponseDto })
+  async signup(@Body() signupDto: SignupRequestDto) {
+    const signupUserIdx = await this.authService.signup(signupDto);
+
+    return ResponseEntity.SUCCESS_WITH({
+      userIdx: signupUserIdx,
+    });
+  }
 }
--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/common/prisma/prisma.service';
+import { SignupRequestDto } from './dto/signup.dto';
+import * as bcrypt from 'bcrypt';
+import { User } from '../user/entitites/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async signup(signupDto: SignupRequestDto): Promise<User['idx']> {
+    const hashedPassword = await bcrypt.hash(signupDto.password, 10);
+
+    if (await this.checkDuplicateEmail(signupDto.email)) {
+      throw new ConflictException('이미 사용 중인 이메일입니다.');
+    }
+
+    if (await this.checkDuplicateLoginId(signupDto.loginId)) {
+      throw new ConflictException('이미 사용 중인 아이디입니다.');
+    }
+
+    const createUserResult = await this.prismaService.user.create({
+      data: {
+        ...signupDto,
+        password: hashedPassword,
+        provider: signupDto.provider,
+      },
+      select: {
+        idx: true,
+      },
+    });
+
+    return createUserResult.idx;
+  }
+
+  private async checkDuplicateEmail(email: string): Promise<boolean> {
+    const checkDuplicateEmail = await this.prismaService.user.findFirst({
+      select: {
+        idx: true,
+      },
+      where: {
+        email,
+        deletedAt: null,
+      },
+    });
+
+    return !!checkDuplicateEmail;
+  }
+
+  private async checkDuplicateLoginId(loginId: string): Promise<boolean> {
+    const checkDuplicateLoginId = await this.prismaService.user.findFirst({
+      select: {
+        idx: true,
+      },
+      where: {
+        loginId,
+        deletedAt: null,
+      },
+    });
+
+    return !!checkDuplicateLoginId;
+  }
+}
